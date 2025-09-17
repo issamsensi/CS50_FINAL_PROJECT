54744e4e7d430c8769a41d0268803246cc87a30b
ISSAM SENSI <102659862+issamsensi@users.noreply.github.com>
2025-09-17 13:57:00 +0100

    new

--- WEBSITE/app.py
+++ WEBSITE/app.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/developer")
+def developer():
+    return render_template("developer.html")
